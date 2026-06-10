dee9caa0018f3efbc29bb3f4372f591053274d85
Samuel El Zaghir <elzaghirsamuel@gmail.com>
2026-06-10 04:27:44 -0500

    Instalar libreria Google Authenticator

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '75fd7c9291d4df39b8603debae476345a8a423ab',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '75fd7c9291d4df39b8603debae476345a8a423ab',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'sonata-project/google-authenticator' => array(
+            'pretty_version' => '2.3.1',
+            'version' => '2.3.1.0',
+            'reference' => '71a4189228f93a9662574dc8c65e77ef55061b59',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../sonata-project/google-authenticator',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
